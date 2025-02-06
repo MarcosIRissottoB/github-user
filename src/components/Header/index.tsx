9b2c360a08a98adac2867ca4bd76b18265d80378
Marcos Rissotto Britos <marcosrissottob@gmail.com>
2025-02-06 10:00:13 -0300

    Remove fake error in headet

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,33 @@ import { useUsers } from '@/context/UsersContext';
 import Search from '@/components/search';
 
 const Header: React.FC = () => {
-  throw new Error('Error intencional');
+  const [query, setQuery] = useState('');
+  const { searchUsers, isLoading, error } = useSearchUsers();
+  const { setUsers } = useUsers();
+
+  const handleSearch = async () => {
+    if (query.length > 2) {
+      const { data } = await searchUsers(query);
+      setUsers(data || []);
+    } else if (query.length === 0) {
+      setUsers([]);
+    }
+  };
+
+  return (
+    <header className={styles.header}>
+      <Link href="/" className={styles.header__title}>
+        <h1 className={styles.header__title}>GITHUB USERS</h1>
+      </Link>
+      <Search
+        query={query}
+        setQuery={setQuery}
+        handleSearch={handleSearch}
+        isLoading={isLoading}
+        error={!!error}
+      />
+    </header>
+  );
 };
 
 export default Header;
